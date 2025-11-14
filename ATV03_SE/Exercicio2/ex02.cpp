75d26825048297d7a179283c11724656446db52c
ViniciusMachado <viniciusmachadofragamatos8@gmail.com>
2025-11-14 20:17:32 -0300

    Update ex02.cpp

--- ATV03_SE/Exercicio2/ex02.cpp
+++ ATV03_SE/Exercicio2/ex02.cpp
@@ -1,12 +1,19 @@
-int led =9;
-float angulo = 0;
-void setup(){
-    pinMode(led, OUTPUT);
+int led = 9;          // Pino PWM
+float angulo = 0;     // Ângulo inicial
+float velocidade = 0.05;  // Controla a velocidade da respiração (quanto menor, mais lento)
+
+void setup() {
+  pinMode(led, OUTPUT);
 }
-void loop(){
-    float valor = sin(angulo) * 127.5 + 127.5;
-    analogWrite(led, valor);
-    angulo += 0.1;
-    if (angulo > 2 * PI) angulo = 0;
-    delay(30);
+
+void loop() {
+  // Gera valor de 0 a 255
+  float valor = (sin(angulo) + 1) * 127.5;  
+  analogWrite(led, (int)valor);
+
+  // Atualiza o ângulo
+  angulo += velocidade;
+  if (angulo > TWO_PI) angulo -= TWO_PI;  // Mantém o ângulo dentro de 0 a 2π
+
+  delay(20);  // Ajuste fino da velocidade
 }
