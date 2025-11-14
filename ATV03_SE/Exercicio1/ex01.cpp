113ddbc9f79dbce999b389508979f909e71e3010
ViniciusMachado <viniciusmachadofragamatos8@gmail.com>
2025-11-14 19:57:56 -0300

    Update ex01.cpp

--- ATV03_SE/Exercicio1/ex01.cpp
+++ ATV03_SE/Exercicio1/ex01.cpp
@@ -1,16 +1,45 @@
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Inicio da leitura");
 }
 
 void loop() {
-    int leitura = analogRead(A0);
-    float media = (leitura + leitura + leitura) / 3.0;
-    float raiz = sqrt(media);
+  //ler 3 valores reais com intervalo de 1 segundo
+  Serial.println("Realizando 3 leituras...");
+  int leitura = analogRead(A0);
+  delay(1000);
+  
+  int leitura2 = analogRead(A0);
+  delay(1000);
+  
+  int leitura3 = analogRead(A0);
+  delay(1000);
+  
+  //Calculo da media
+  float media = (leitura + leitura2 + leitura3) / 3.0;
+  
+  //raiz quadrada da media
+  float raiz = sqrt(media);
 
-    Serial.print("Media");
-    Serial.println(round(media));
-    Serial.print("Raiz quadrada da media:");
-    Serial.println(ceil(raiz));
+  //valor ao quadrado da media
+  float quadrado = pow(media, 2);
+  
+  //resultados
+  Serial.println("\n--- resultado das contas ---");
+  
+  //leituras
+  Serial.print("Leituras: ");
+  Serial.print(leitura);Serial.print(", ");
+  Serial.print(leitura2);Serial.print(", ");
+  Serial.print(leitura3);
+  
+  
+  Serial.print("Media: ");
+  Serial.print(media);
+  Serial.print("Media Arredondada");
+  Serial.println(round(media));
+  Serial.print("Raiz quadrada da media: ");
+  Serial.println(ceil(raiz));
 
-    delay(2000);
+  delay(2000);
 }
